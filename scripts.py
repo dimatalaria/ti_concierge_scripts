4335aac3c693a47f9fd4143aba6e44b18d4c2854
dima <prostodima992@gmail.com>
2025-01-20 16:44:47 +0300

    upload

--- scripts.py
+++ scripts.py
@@ -1,6 +1,6 @@
 import openpyxl
 
-keynect = openpyxl.load_workbook("keystrom.xlsx")
+keynect = openpyxl.load_workbook("losika53.xlsx")
 
 sheet = keynect.active
 
@@ -8,9 +8,14 @@ value_key = sheet["A1"].value
 
 cell_key = sheet.cell(row=1, column=1).value
 sps = []
-for row in sheet.iter_rows(min_row=1, max_row=8, values_only=True):
-    json = {"CardNo":row[0],"UserID":row[1],"UserName":"1","VTOPosition":"","CardType":0,"CardStatus":0}
+for row in sheet.iter_rows(min_row=1, max_row=606, values_only=True):
+    num = row[0]
+    hex_number = f"{num:08X}"
+    bytes_list = [hex_number[i:i + 2] for i in range(0, len(hex_number), 2)]
+    reversed_bytes = ''.join(bytes_list[::-1]).upper()
+    json = {"CardNo":reversed_bytes, "UserID":row[1], "UserName":"53", "VTOPosition":"", "CardType":0, "CardStatus":0}
     sps.append(json)
+
 
 file = open("key.txt", "w")
 file.write(str(sps))
--- scripts_write.py
+++ scripts_write.py
@@ -1,6 +1,6 @@
 import openpyxl
 
-keynect = openpyxl.load_workbook("keystrom.xlsx")
+keynect = openpyxl.load_workbook("losika53.xlsx")
 
 sheet = keynect.active
 
@@ -8,10 +8,10 @@ i = 1
 
 num = 1
 
-while i <= 100:
+while i <= 606:
     sheet.cell(row=i, column=2, value=num)
     sheet.cell(row=i+1, column=2, value=num)
     i += 2
     num += 1
 
-keynect.save("keystrom.xlsx")
+keynect.save("losika53.xlsx")
